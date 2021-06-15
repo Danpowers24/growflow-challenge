6922b903db4d2180343e9e1c3ca9993c45abec46
Danpowers24 <danpowers610@gmail.com>
2021-06-14 22:45:11 -0700

    Add a few options to server.js remove warnings - deprecated string parser and useUnifiedTopology

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
-// var express = require('express'),
-//   app = express(),
-//   port = process.env.PORT || 3000;
-
-// app.listen(port);
-
-// console.log('GrowFlow Challenge API server running on port:' + port);
-
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Customer = require('./api/models/challengeModel'), //created model loading here
-  bodyParser = require('body-parser');
+  Customer = require('./api/models/challengeModel'); //created model loading here
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Phoenixdb'); 
+mongoose.connect('mongodb://localhost/Phoenixdb', { useNewUrlParser: true, useUnifiedTopology: true }); 
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+// this replaces the bodyParser method
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 var routes = require('./api/routes/challengeRoutes'); //importing route
 routes(app); //register the route
